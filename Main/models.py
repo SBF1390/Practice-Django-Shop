b7d97789ec18dda4227fd5de60a52711af4492d6
SBF1390 <sbf1390@gmail.com>
2025-09-08 08:42:12 +0330

    Table Member Added To DataBase

--- Main/models.py
+++ Main/models.py
@@ -1,2 +1,9 @@
 from django.db import models
 
+class Member(models.Model):
+    FullName = models.CharField(max_length=100)
+    Username = models.CharField(max_length=100,unique=True)
+    Password = models.CharField(max_length=50)
+    Email = models.EmailField(max_length=254)
+    def __str__(self):
+        return f"{self.FullName} ({self.pk})"
