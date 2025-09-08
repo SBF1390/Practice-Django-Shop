4c5b839cb577dc903eeba57d6b85e20bf9138058
SBF1390 <sbf1390@gmail.com>
2025-09-08 09:17:29 +0330

    Cart Table Added To DataBase And Models

--- Main/models.py
+++ Main/models.py
@@ -1,15 +1,27 @@
 from django.db import models
 
-class Member(models.Model):
-    FullName = models.CharField(max_length=100)
-    Username = models.CharField(max_length=100,unique=True)
-    Password = models.CharField(max_length=50)
-    Email = models.EmailField(max_length=254)
-    def __str__(self):
-        return f"{self.FullName} ({self.pk})"
 
 class Products(models.Model):
     Name = models.CharField(max_length=50)
     Price = models.IntegerField()
     Quantity = models.IntegerField()
     Is_Available = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.Name} ({self.pk})"
+
+
+class Cart(models.Model):
+    Products = models.ManyToManyField(Products)
+    Total_price = models.IntegerField()
+
+
+class Member(models.Model):
+    FullName = models.CharField(max_length=100)
+    Username = models.CharField(max_length=100, unique=True)
+    Password = models.CharField(max_length=50)
+    Email = models.EmailField(max_length=254)
+    Cart = models.ForeignKey(Cart, related_name="Members", on_delete=models.CASCADE,null=True)
+
+    def __str__(self):
+        return f"{self.FullName} ({self.pk})"
